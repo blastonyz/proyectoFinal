3cab8c21c512a2840a47552debaf1bfd93281fe9
blastonyz <blastonyzamora@gmail.com>
2023-12-06 01:31:08 -0300

    map2.01

--- src/routes/views/cartRoutes.js
+++ src/routes/views/cartRoutes.js
@@ -141,8 +141,8 @@ router.get('/carts/:cid', async (req,res) => {
 const cId = req.params.cid;
  const cartDb = await CartModel.findById({_id: cId}).populate('products.prodId');
   console.log(cartDb);
-  const result = cartDb.products.map((prod) =>{return{id:prod.prodId?._id, title:prod.prodId?.title, description:prod.prodId?.description, price:prod.prodId?.price,category: prod.prodId?.category, code: prod.prodId?.code, sotck: prod.prodId?.stock,statusP: prod.prodId?.statusP}})
- res.render('cart' , { title: 'cart',result});
+  const result = cartDb.products.map(prod =>{return{id:prod.prodId?._id, title:prod.prodId?.title, description:prod.prodId?.description, price:prod.prodId?.price,category: prod.prodId?.category, code: prod.prodId?.code, sotck: prod.prodId?.stock,statusP: prod.prodId?.statusP}})
+ res.render('cart' , { title: 'cart',cartDb: result});
 });
 
 
