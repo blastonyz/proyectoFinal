4f9e10830582cb4f43544d70cbe3e13294a12648
blastonyz <blastonyzamora@gmail.com>
2024-01-25 15:15:52 -0300

    promise.all

--- src/services/tickets.controller.js
+++ src/services/tickets.controller.js
@@ -3,7 +3,7 @@ import TicketsDao from "../dao/tickets.dao.js";
 import CartController from "../controller/carts.controller.js";
 import ProductController from "../controller/products.controller.js";
 import { v4 as uuidv4 } from 'uuid';
-
+/*
 const ticketsRepository = new TicketsRepository(TicketsDao);
 
 export default class TicketsController{
@@ -67,5 +67,78 @@ export default class TicketsController{
     } catch (error) {
         console.error('Error:', error);
     }
+    }
+}
+*/
+
+
+const ticketsRepository = new TicketsRepository(TicketsDao);
+
+export default class TicketsController {
+    static async Purchase(cartId, email) {
+        try {
+            const cart = await CartController.getPopulate(cartId);
+            console.log('cart', cart);
+            const productList = await ProductController.get();
+
+            const stockOperations = cart.products.map(async (productInCart) => {
+                const productInList = productList.find(p => p._id.toString() === productInCart.prodId._id.toString());
+
+                if (!productInList) {
+                    console.error(`Product with prodId ${productInCart.prodId} not found in the product list.`);
+                   
+                }
+
+                if (productInCart.quantity < productInList.stock) {
+                    const newStock = productInList.stock - productInCart.quantity;
+                    const data = {
+                        title: productInList.title,
+                        description: productInList.description,
+                        price: productInList.price,
+                        category: productInList.category,
+                        code: productInList.code,
+                        stock: newStock,
+                        StatusP: productInList.statusP,
+                        thumbnail: productInList.thumbnail
+                    };
+                    const _id = productInList._id.toString();
+                    return {
+                        price: productInList.price * productInCart.quantity,
+                        updateProduct: ProductController.findAndUpdate(_id, data)
+                    };
+                } else {
+                    return {
+                        prodId: productInCart.prodId,
+                        quantity: productInCart.quantity
+                    };
+                }
+            });
+           
+            const stockResults = await Promise.all(stockOperations);
+            console.log('stocksResults',stockResults);
+            
+            const okStock = stockResults
+                .filter(result => result && result.price)
+                .map(result => result.price);
+            console.log('okstock',{okStock})    
+            const notStock = stockResults
+                .filter(result => result && !result.price)
+                .map(result => ({ prodId: result.prodId, quantity: result.quantity }));
+                console.log('notstock',{notStock});
+            // total de compra
+            const total = okStock.reduce((acc, val) => acc + val, 0);
+            const purchaseData = {
+                code: uuidv4(),
+                amount: total,
+                purchaser: email
+            };
+
+           
+
+            await CartController.updateCart({ _id: cartId }, notStock);
+            return await ticketsRepository.create(purchaseData);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 }
